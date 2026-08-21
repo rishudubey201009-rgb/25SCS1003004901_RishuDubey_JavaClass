c0103640242ab7cd84b52354d9aa9a507f8ff566
Rishu Dubey <rishudubey201009@gmail.com>
2026-08-21 11:16:51 +0530

    fixing errro

--- Assignment4/Program4.java
+++ Assignment4/Program4.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 public class Program4 {
      public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.print("Enter a number: ");
         int n=sc.nextInt(),i,a=1;
-        for(i=2;i<=n/2;i++){
+        if (n==1||n==0){
+          System.out.println("not prime");
+        }
+        else
+          {for(i=2;i<=n/2;i++){
              if (n%i==0){
                 System.out.println("not prime");
                  a=0;
@@ -12,7 +16,8 @@ public class Program4 {
                 }
             }
             if(a!=0)
-                System.out.println("prime ");  
+                System.out.println("prime "); 
+          } 
         sc.close();
           }
 
