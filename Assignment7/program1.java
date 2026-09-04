1e00521b86670db579029a08c0471b67d90609b5
Rishu Dubey <rishudubey201009@gmail.com>
2026-09-04 09:59:36 +0530

    upgrading previous code

--- Assignment7/program1.java
+++ Assignment7/program1.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class BankAccount {
     private String accountNumber;
     private String accountHolderName;
@@ -37,18 +38,22 @@ class BankAccount {
             System.out.println("Invalid Balance");   
     }
     public void deposit(double amount) {
-        if (amount > 0) 
+        if (amount > 0){
             balance = balance + amount;
+        System.out.println("Updated Balance: " + getBalance());
+         }
         else 
             System.out.println("Invalid Deposit Amount");
     }
     public void withdraw(double amount) {
         if (amount <= 0)
-            System.out.println("Invalid Withdrawal Amount");
+            System.out.println("Invalid Withdrawal Amount ");
         else if (amount > balance) 
             System.out.println("Insufficient Balance");
-        else 
+        else{
             balance = balance - amount;
+            System.out.println("Updated Balance: " + getBalance());
+         }
     }
     public void displayAccountDetails() {
         System.out.println("Account Number: " + accountNumber);
@@ -58,13 +63,25 @@ class BankAccount {
 }
 public class program1 {
     public static void main(String[] args) {
-        BankAccount account =
-                new BankAccount("ACC1001", "Rishu Dubey", 5000.0);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter you name "); 
+        String Name=sc.nextLine();  
+        System.out.print("Enter you Account number ");
+        String account_number=sc.nextLine();    
+        BankAccount account = new BankAccount(account_number, Name, 5000.0);
+        System.out.print("1)Deposit,2)Withdrawal\nFor deposit enter 1 and for Withdrawal enter 2: "); 
+        int a=sc.nextInt();
         account.displayAccountDetails();
-        System.out.println("Depositing: 2000");
-        account.deposit(2000);
-        System.out.println("Withdrawal: 3000");
-        account.withdraw(3000);
-        System.out.println("Updated Balance: " + account.getBalance());
+        if(a==1){
+        System.out.print("Inter the amount you want to Deposit "); 
+        int x=sc.nextInt();
+        System.out.println("Depositing:"+ x);
+        account.deposit(x);
+        }else if (a==2){
+        System.out.print("Enter the amount you want to withgraw "); 
+        int y=sc.nextInt();
+            System.out.println("Withdrawal:" + y);
+        account.withdraw(y);
+        }else System.out.print("invalide input");
     }
 }
